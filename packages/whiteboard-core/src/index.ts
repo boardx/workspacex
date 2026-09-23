42c0b45f1495f38d260bd7845cc4d33b985ee7b3
usamshen <usam.shen@gmail.com>
2026-09-24 06:55:01 +0800

    feat(board): add Yjs document kernel (#3945)

---
+++ packages/whiteboard-core/src/index.ts
@@ -0,0 +1,6 @@
+export { createWhiteboardDocument, cloneDocument, readObjects, validateDocument, executeCommands, copyObjects } from './document';
+export { WhiteboardUndo } from './undo';
+export { WhiteboardObject, WhiteboardGeometry, WhiteboardStyle, WhiteboardCommand, WhiteboardCommandBatch, WHITEBOARD_LIMITS } from '@repo/contracts/whiteboard-document';
+export { prepareWhiteboardUpdate, WHITEBOARD_UPDATE_LIMITS } from './update';
+
+
