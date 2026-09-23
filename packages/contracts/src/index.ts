bddaae143e227df5cddfd228d02118e93e86ca14
usamshen <usam.shen@gmail.com>
2026-09-24 04:15:58 +0800

    feat(board): persist private resources and member roles (#3926)

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -111,3 +111,4 @@ export * as postinvestRating from "./postinvest-rating";
  * R7-1 的最终归宿是 F01 的 skill 包，包建起来后本文件搬家、不再抄第二份。 */
 export * as postinvestRatingRules from "./postinvest-rating-rules";
 
+export * as whiteboard from './whiteboard';
